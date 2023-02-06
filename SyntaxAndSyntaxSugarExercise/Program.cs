d5195581d1603af8c12c63d702a2af2a798aa729
Brody May <brodymay2019@outlook.com>
2023-02-06 15:02:24 -0500

    fixed

--- SyntaxAndSyntaxSugarExercise/Program.cs
+++ SyntaxAndSyntaxSugarExercise/Program.cs
@@ -16,6 +16,7 @@
             }
             
             var response2 = (answer < 9) ? $"{answer} is less than nine" : $"{answer} is greater than or equal to nine";
+            Console.WriteLine(response);
         }
     }
 }
